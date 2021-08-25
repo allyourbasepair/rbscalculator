30c36a7137bb2df26846b129755240b0f0746c70
Vivek Raja <vivek.r.raja@gmail.com>
2021-08-26 00:36:44 +0400

    comment out code which requires `rbs_calculator` subpackage. Needs to be uncommented when package name is fixed.

--- model/datasets/datasets.go
+++ model/datasets/datasets.go
@@ -1,52 +1,68 @@
-package model
+package datasets
 
 import (
 	"embed"
-	"fmt"
 	"sync"
 
-	"github.com/allyourbasepair/rbscalculator"
 	"github.com/allyourbasepair/rbscalculator/csv_helper"
 )
 
 /***********************************************
 Functions for modifying datasets in the `datasets` directory.
-Currenlty only includes a func `add16SrRNA` to add the 16S
+
+Currently only includes a func `add16SrRNA` to add the 16S
 rRNA of an organism to a dataset.
 ***********************************************/
 
-func add16SrRNA(fs embed.FS, csvFile string, outputCSVFile string, organismColIdx, rrnaColIdx int) {
+//go:embed *
+var EmbeddedDatasetsDirectory embed.FS
+
+// add16SrRNA adds the 16s rRNA (based on the
+// `rbs_calculator.Organism16SrRNAMap` map) to a dataset
+func add16SrRNA(datasetName string, organismColIdx int) {
+	dataset := datasetName + ".csv"
+
+	// Create a wait group that will only 'release' when all go subroutines
+	// call `wg.Done()`
 	var wg sync.WaitGroup
 
 	nbHeaderRows := 1
-	headerRow := csv_helper.ReadHeader(fs, csvFile, nbHeaderRows)[0]
-	headerRow[rrnaColIdx] = "16S rRNA"
+	headerRow := csv_helper.ReadHeader(EmbeddedDatasetsDirectory, dataset, nbHeaderRows)[0]
+	headerRow = append(headerRow, "16S rRNA")
 
-	// create a channel for csv output
-	csvOutputChannel := make(chan []string, 1)
-
-	// write all header row to file
-	csvOutputChannel <- headerRow
-
-	// add to wait group for new csv rows channel
-	wg.Add(1)
+	// Step 1: populate a channel with each row of the dataset
 	csvRowsChannel := make(chan []string)
-	go csv_helper.ReadCSV(fs, csvFile, csvRowsChannel, &wg, nbHeaderRows)
-
+	// add to the wait group for the `csv_helper.ReadCSV` subroutine
 	wg.Add(1)
-	go doAdd16SrRNA(csvRowsChannel, organismColIdx, rrnaColIdx, csvOutputChannel, &wg)
+	go csv_helper.ReadCSV(EmbeddedDatasetsDirectory, dataset, csvRowsChannel, &wg, nbHeaderRows)
 
+	// Step 2: For each row in the dataset, add the 16s rRNA of the organism
+	csvOutputChannel := make(chan []string, 1)
+	// write the header row to file
+	csvOutputChannel <- headerRow
+	// add to the wait group for the `doAdd16SrRNA` subroutine
 	wg.Add(1)
-	go csv_helper.WriteToCSV(outputCSVFile, csv_helper.CREATE, csvOutputChannel, &wg)
+	go doAdd16SrRNA(csvRowsChannel, organismColIdx, csvOutputChannel, &wg)
 
+	// Step 3: Output data from `csvOutputChannel` to a CSV file
+	datasetOutputFile := "./with_16s_rRNA/" + datasetName + ".csv"
+	// add the current unix timestamp to the file name
+	datasetOutputFile = csv_helper.FileNameWithUNIXTimestamp(datasetOutputFile)
+	// add to the wait group for the `csv_helper.WriteToCSV` subroutine
+	wg.Add(1)
+	go csv_helper.WriteToCSV(datasetOutputFile, csv_helper.CREATE, csvOutputChannel, &wg)
+
+	// wait till all the subroutines call `wg.Done()`
 	wg.Wait()
 }
 
-func doAdd16SrRNA(csvInputChannel chan []string, organismColIdx, rrnaColIdx int, csvOutputChannel chan []string, wg *sync.WaitGroup) {
-	// var mrnasWithProperties []MRNAWithProperties
+func doAdd16SrRNA(csvInputChannel chan []string, organismColIdx int, csvOutputChannel chan []string, wg *sync.WaitGroup) {
+	// close the csv output channel when this func returns
 	defer close(csvOutputChannel)
+	// call `wg.Done()` to decrement the wait group counter when this func returns
 	defer wg.Done()
 
+	// weird Go syntax to wait for a message from a channel
 	for {
 		select {
 		case csvRow, ok := <-csvInputChannel:
@@ -54,13 +70,14 @@ func doAdd16SrRNA(csvInputChannel chan []string, organismColIdx, rrnaColIdx int,
 				return
 			}
 
-			organism := csvRow[organismColIdx]
+			// get the organism for the current dataset row
+			// organism := csvRow[organismColIdx]
 
-			if val, ok := rbs_calculator.Organism16SrRNAMap[organism]; ok {
-				csvRow[rrnaColIdx] = val
-			} else {
-				panic(fmt.Errorf("do not have 16S rRNA for organism: %v", organism))
-			}
+			// if val, ok := rbs_calculator.Organism16SrRNAMap[organism]; ok {
+			// 	csvRow = append(csvRow, val)
+			// } else {
+			// 	panic(fmt.Errorf("do not have 16S rRNA for organism: %v", organism))
+			// }
 
 			csvOutputChannel <- csvRow
 		}
